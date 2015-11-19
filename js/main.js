fd0aa56683f65c34eeb825ae6a1489954754a646
[yarhtut@live.com] <yhtut@android-b1b6d7c3cfd37a1a.wgtn.silverstripe.com>
2015-11-20 10:24:06 +1300

    added in navigation menu that sticks on scroll

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-//navbar-collapse hide if select 
+//navbar-collapse hide if select
 $(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
         $(this).collapse('hide');
     }
 });
+
+
+$(function(){
+        // Check the initial Poistion of the Sticky Header
+        var stickyHeaderTop = $('#main-nav-fixed').offset().top;
+
+        $(window).scroll(function(){
+                if( $(window).scrollTop() > stickyHeaderTop ) {
+                        $('#main-nav-fixed ').css({position: 'fixed', top: '0px'});
+                        $('#nav-seperator').css('display', 'block');
+                } else {
+                        $('#main-nav-fixed ').css({position: 'static', top: '0px'});
+                        $('#nav-seperator').css('display', 'none');
+                }
+        });
+  });
